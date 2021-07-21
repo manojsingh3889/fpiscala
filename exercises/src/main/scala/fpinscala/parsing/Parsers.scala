ed7e6e25110826a5f9d4e274fa688846b9eef213
wen.zhang <wen.zhang@klarna.com>
2021-07-21 18:47:14 +0200

    Exercise 9.1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,45 @@
 package fpinscala.parsing
 
+import fpinscala.testing.Prop.forAll
+import fpinscala.testing.{Gen, Prop}
+
 import language.higherKinds
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def run[A](p: Parser[A])(input: String): Either[ParseError,A]
+  def char(c: Char): Parser[Char]
+
+  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C]
+  def map2WithProduct[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] = {
+    map(product(p, p2))(a => f(a._1, a._2))
+  }
+  def productWithMap2[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)] =
+    map2(p, p2)((a, b) => (a, b))
+//  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)] = {
+//    map(p)(a => map(p2)(b => (a, b)))
+//  }
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  //aaa
+  def many1[A](p: Parser[A]): Parser[List[A]] = {
+    map2(p, many(p))((a, b) => a :: b)
+  }
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+  ParserOps[String] = ParserOps(f(a))
   case class ParserOps[A](p: Parser[A]) {
-
-
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+//    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop = equal(p, p.map(a => a))(in)
   }
 }
 
